93dec5da3fbe1912d3a03f271eb9ae80f4773735
Daniel Jajliardo <tntwzrd.minecraft@gmail.com>
2017-01-20 23:43:40 -0500

    Fixed repo

---
+++ LogManagerV2/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "src\smallUtils.h"
+#include "src\logManager.h"
+
+using namespace std;
+
+int main() {
+	
+	logger::LogManager Logs = logger::LogManager::get(logger::DefualtLogs::Info, logger::CustomLogs::CustomOn);
+
+	Logs.LOGINFO("Info Log!!");
+	Logs.LOGWARNING("Warning Log!!");
+	Logs.LOGERROR("Error Log!!");
+	Logs.LOGDEBUG("Debug Log!!");
+	Logs.LOGCUSTOM("CUSTOM", "Custom Log!!");
+	Logs.LOGCUSTOM("CUSTOM", "Custom Log!!", 0);
+
+
+	cout << endl;
+	system("PAUSE");
+	return 0;
+}
